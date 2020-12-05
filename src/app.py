6cacc4b8946eda1c286d06a6c7bee0aa13235313
Hugo Johnsson <hugo.bjarred@gmail.com>
2020-12-05 10:59:37 +0100

    removed insert endpoint as it is not used anymore

--- src/app.py
+++ src/app.py
@@ -17,23 +17,12 @@ for line in lines:
         prefix_tree_service.insert(word)
 
 
+# Endpoint used to get all matching phrases in the prefix tree
 @app.route("/api/v1/match/<prefix>")
 def match(prefix):
     matches = prefix_tree_service.get_matching_phrases(prefix)
 
     return jsonify(matches)
-
-@app.route("/api/v1/insert/<phrase>")
-def insert(phrase):
-    res = {"success": False}
-
-    try:
-        prefix_tree_service.insert(phrase)
-        res["success"] = True
-    except:
-        pass
-
-    return jsonify(res)    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv("PORT"))
