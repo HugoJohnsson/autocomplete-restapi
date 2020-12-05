e10b4b53564aa71f87056c7dbf3217f666d1e501
Hugo Johnsson <hugo.bjarred@gmail.com>
2020-12-05 10:55:46 +0100

    downloaded a file with english words, when you start the flask app the words will be loaded into memory (into the prefix tree)

--- src/app.py
+++ src/app.py
@@ -6,6 +6,17 @@ from service.prefix_tree_service import prefix_tree_service
 # Initialize the Flask app instance
 app = Flask(__name__)
 
+# Load all words in the database into the prefix tree
+words_file = open("words.txt", "r")
+lines = words_file.readlines()
+
+for line in lines:
+    word = line.strip()
+    
+    if len(word) > 2:
+        prefix_tree_service.insert(word)
+
+
 @app.route("/api/v1/match/<prefix>")
 def match(prefix):
     matches = prefix_tree_service.get_matching_phrases(prefix)
